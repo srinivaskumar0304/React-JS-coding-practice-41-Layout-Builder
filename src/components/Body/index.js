42798fb7d7f9caed9ce18a660b8d434b556307a8
Srinivas kumar <seenukumar124@gmail.com>
2022-11-23 07:44:51 +0000

    React JS coding practice 41 Layout Builder

---
+++ src/components/Body/index.js
@@ -0,0 +1,48 @@
+// Write your code here
+import ConfigurationContent from '../../context/ConfigurationContext'
+
+import './index.css'
+
+const Body = () => (
+  <ConfigurationContent.Consumer>
+    {value => {
+      const {showContent, showLeftNavbar, showRightNavbar} = value
+      return (
+        <div className="body-container">
+          {showLeftNavbar ? (
+            <div className="left-navbar">
+              <h1 className="body-header-text">Left Navbar Menu</h1>
+              <ul className="list-items">
+                <li className="item">Item 1</li>
+                <li className="item">Item 2</li>
+                <li className="item">Item 3</li>
+                <li className="item">Item 4</li>
+              </ul>
+            </div>
+          ) : null}
+          {showContent ? (
+            <div className="content-container">
+              <h1 className="body-header-text">Content</h1>
+              <p className="content-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                elusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam.
+              </p>
+            </div>
+          ) : null}
+          {showRightNavbar ? (
+            <div className="right-navbar-container">
+              <h1 className="body-header-text">Right Navbar</h1>
+              <div className="box-container">
+                <p className="box">Ad 1</p>
+                <p className="box">Ad 2</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      )
+    }}
+  </ConfigurationContent.Consumer>
+)
+
+export default Body
